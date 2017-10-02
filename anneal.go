261aefb885401a3d4c9be4a7f345d85fb7db3cb1
Dan McCandless <dkmccandless@gmail.com>
2017-10-02 22:34:13 +0800

    Add Schedule to control annealing

--- anneal.go
+++ anneal.go
@@ -6,12 +6,6 @@ import (
 	"math/rand"
 )
 
-var (
-	maxi int = 1e6
-	ti       = 1.
-	tf       = 1e-5
-)
-
 // A State can undergo simulated annealing optimization.
 type State interface {
 	// Energy returns the energy of a State.
@@ -23,15 +17,35 @@ type State interface {
 	Neighbor() State
 }
 
+// A Schedule controls the annealing process.
+type Schedule struct {
+	Iter int     // number of iterations
+	Ti   float64 // initial temperature, as a multiple of the input State's energy
+	Tf   float64 // final temperature, as a multiple of the input State's energy
+}
+
+// NewSchedule returns a pointer to a Schedule populated with default values.
+func NewSchedule() *Schedule {
+	return &Schedule{
+		Iter: 1e6,
+		Ti:   1,
+		Tf:   1e-5,
+	}
+}
+
 // Anneal implements simulated annealing on the input State and returns the best State encountered during the search.
-func Anneal(s State) State {
+// Once per iteration, it calls s.Neighbor() and then calls Energy() on the neighboring State.
+// The new State is adopted with probability 1 if its Energy e' is lower than the original State's energy e,
+// and with probability exp(-(e'-e)/T) otherwise, where T = Ti * exp(-i/k) is the annealing temperature of the current iteration i,
+// and the scale factor k = Iter / ln(Ti/Tf) is the number of iterations required for the temperature to drop by a factor of e.
+func Anneal(s State, sch *Schedule) State {
 	e := s.Energy()
 	sbest, ebest := s, e
 	var (
-		Tinitial = ebest * ti
-		k        = float64(maxi) / math.Log(ti/tf)
+		Tinitial = e * sch.Ti
+		k        = float64(sch.Iter) / math.Log(sch.Ti/sch.Tf)
 	)
-	for i := 0; i < maxi; i++ {
+	for i := 0; i < sch.Iter; i++ {
 		snew := s.Neighbor()
 		enew := snew.Energy()
 		if enew < e {
@@ -39,8 +53,8 @@ func Anneal(s State) State {
 				sbest, ebest = snew, enew
 			}
 		} else {
-			temp := Tinitial * math.Exp(-float64(i)/k)
-			if p := math.Exp(-(enew - e) / temp); rand.Float64() > p {
+			T := Tinitial * math.Exp(-float64(i)/k)
+			if p := math.Exp(-(enew - e) / T); rand.Float64() > p {
 				continue
 			}
 		}
