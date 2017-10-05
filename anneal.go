9f7f3df7b1afb56451e528741063540fef30c602
Dan McCandless <dkmccandless@gmail.com>
2017-10-05 12:40:55 +0800

    clarify doc comments

--- anneal.go
+++ anneal.go
@@ -1,4 +1,27 @@
-// Package anneal implements simulated annealing optimization of arbitrary data types.
+/*
+Package anneal implements simulated annealing optimization of arbitrary data types.
+
+Simulated annealing is a probabilistic search heuristic to approximate the global optimum
+in a discrete search space by traversing the search space in steps and deciding probabilistically
+whether to transition to a new state based on the difference of the "energies" of the current state
+and new state as compared with the annealing "temperature", which starts high and decreases
+with each successive iteration. The transition decision is thus initially sensitive to large
+differences in energy between different areas of the search space, and this sensitivity becomes
+finer as the temperature decreases.
+
+The quality of the result depends on the following conditions:
+
+  * Energy() must be defined such that smaller energies are better than larger energies.
+  * Neighbor() must randomly select a State from all States that differ from the input State
+    by a minimal alteration (i.e., adjacent to the input State in the search space).
+    The diameter of the search space must be small: Neighbor() must enable transition
+    between any two arbitrary States in a small number of steps.
+  * In analogy with the eponymous metallurgical technique, the Schedule parameters must be chosen
+    such that the initial temperature is large compared to the difference between the energies
+    of typical States, the final temperature is small compared to the difference between
+    adjacent states, and the temperature decreases sufficiently slowly that the system is in
+    approximate thermodynamic equilibrium at all times.
+*/
 package anneal
 
 import (
@@ -13,7 +36,7 @@ type State interface {
 	Energy() float64
 
 	// Neighbor returns a State in the state space chosen randomly from those adjacent to the current State.
-	// The new State must occupy a different location in memory than the current State.
+	// Distinct States must not share memory. For example, do not reuse a slice from one State to a neighbor.
 	Neighbor() State
 }
 
@@ -35,15 +58,15 @@ func NewSchedule() *Schedule {
 
 // Anneal implements simulated annealing on the input State and returns the best State encountered during the search.
 // Once per iteration, it calls s.Neighbor() and then calls Energy() on the neighboring State.
-// The new State is adopted with probability 1 if its Energy e' is lower than the original State's energy e,
-// and with probability exp(-(e'-e)/T) otherwise, where T = Ti * exp(-i/k) is the annealing temperature of the current iteration i,
+// The new State is adopted with probability 1 if its energy E' is lower than the original State's energy E,
+// and with probability exp(-(E'-E)/T) otherwise, where T = Ti * exp(-i/k) is the annealing temperature of the current iteration i,
 // and the scale factor k = Iter / ln(Ti/Tf) is the number of iterations required for the temperature to drop by a factor of e.
 func Anneal(s State, sch *Schedule) State {
 	e := s.Energy()
 	sbest, ebest := s, e
 	var (
-		Tinitial = e * sch.Ti
-		k        = float64(sch.Iter) / math.Log(sch.Ti/sch.Tf)
+		T0 = e * sch.Ti
+		k  = float64(sch.Iter) / math.Log(sch.Ti/sch.Tf)
 	)
 	for i := 0; i < sch.Iter; i++ {
 		snew := s.Neighbor()
@@ -53,7 +76,7 @@ func Anneal(s State, sch *Schedule) State {
 				sbest, ebest = snew, enew
 			}
 		} else {
-			T := Tinitial * math.Exp(-float64(i)/k)
+			T := T0 * math.Exp(-float64(i)/k)
 			if p := math.Exp(-(enew - e) / T); rand.Float64() > p {
 				continue
 			}
